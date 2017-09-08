56e010cd37e74bd910ca1d119327b62fe2c8f072
Janley Zhang (Shang Hai Wei Chuang Ruan Jian) <v-lezha1@microsoft.com>
2017-09-08 12:31:22 +0800

    logs

---
+++ MyWindowsService1/MyWindowsService1/ServiceTest1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyWindowsService1
+{
+    public partial class ServiceTest1 : ServiceBase
+    {
+        public ServiceTest1()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            using (System.IO.StreamWriter sw = new System.IO.StreamWriter("D:\\log.txt", true))
+            {
+                sw.WriteLine(DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss ") + "Start.");
+            }
+        }
+
+        protected override void OnStop()
+        {
+            using (System.IO.StreamWriter sw = new System.IO.StreamWriter("D:\\log.txt", true))
+            {
+                sw.WriteLine(DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss ") + "Stop.");
+            }
+        }
+    }
+}
