0abde23480f96848099666df5f9907935450e6cb
Billycool1020 <billyliu.cool@163.com>
2017-11-21 17:57:25 +0800

    forrevert

--- WSWebTool/WSWebTool/Models/SHP.Designer.cs
+++ WSWebTool/WSWebTool/Models/SHP.Designer.cs
@@ -1,3 +1,4 @@
+﻿// T4 code generation is enabled for model 'D:\WSToolProject\WSWebTools\WSWebTool\WSWebTool\Models\SHP.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
